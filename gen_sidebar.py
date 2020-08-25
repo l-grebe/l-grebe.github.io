74ea450e66769c566480b701de459d771eb3f4f7
husy <shengyong.hu@17zuoye.com>
2020-08-25 16:33:22 +0800

    123

--- gen_sidebar.py
+++ gen_sidebar.py
@@ -20,8 +20,8 @@ def get_file_title(file_path):
                 show_name = line
                 break
     if os.path.isfile(file_path):
-        dir_name = file_path.split('/')[-2]
-        file_name = file_path.split('/')[-1]
+        dir_name = os.path.basename(os.path.dirname(file_path))
+        file_name = os.path.basename(file_path)
         if file_name.startswith('p') and str.isdigit(file_name[1]):
             chapter_num = int(''.join(filter(str.isdigit, dir_name)))
             file_idx = int(''.join(filter(str.isdigit, file_name.split('_', 1)[0])))
@@ -40,7 +40,6 @@ def list_files(path, level):
         d_path_readme = os.path.join(d_path, 'README.md')
         show_name = get_file_title(d_path_readme)
         show_path = d_path.split('/', 1)[-1] + '/'
-        # print(gen_row(show_name, show_path, level), end='')
         yield gen_row(show_name, show_path, level)
         for row in list_files(os.path.join(path, d), level + 1):
             yield row
@@ -54,15 +53,10 @@ def list_files(path, level):
         f_path = os.path.join(path, f)
         show_name = get_file_title(f_path)
         show_path = f_path.split('/', 1)[-1]
-        # print(gen_row(show_name, show_path, level), end='')
         yield gen_row(show_name, show_path, level)
 
 
-def main():
+if __name__ == '__main__':
     with open('_sidebar.md', 'w') as fd:
         for row in list_files('.', 0):
             fd.write(row)
-
-
-if __name__ == '__main__':
-    main()
